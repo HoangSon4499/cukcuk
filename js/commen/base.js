db0e5b0e006409424ddcd6fb34bc75b7e668457e
HoangSon4499 <nguyenhoangson4499@gmail.com>
2020-12-31 17:24:51 +0700

    đang làm baseJS, cần hoàn thiện nốt fieldName trong thẻ <th> employee.html

---
+++ js/commen/base.js
@@ -0,0 +1,13 @@
+﻿class BaseJS {
+    constructor() {
+        this.loadData();
+    }
+
+    loadData() {
+        // lấy thông tin các cột dữ liệu
+        var ths = $('table thead th');
+        $.each(ths, function (index, item) {
+
+        })
+        // lấy thông tin dữ liệu sẽ map tương ứng với các cột
+}
---
+++ js/commen/commen.js
@@ -0,0 +1,34 @@
+﻿/**---------------------------------------------
+ * format dữ liệu ngày tháng sang ngày tháng năm
+ * creatBy: nhson(31/12/2020)
+ * @param {any} date có kiểu dữ liệu bất kỳ
+ */
+function formatDate(date) {
+    var date = new Date(date);
+    if (Number.isNaN(date.getDate())) {
+        return "";
+    } else {
+        var day = date.getDate();
+        var month = date.getMonth() + 1;
+        var year = date.getFullYear();
+        day = day < 10 ? '0' + day : day;
+        month = month < 10 ? '0' + month : month;
+        var formatDate = day + "/" + month + "/" + year;
+        return formatDate;
+    }
+}
+
+/**----------------------------------
+ * Hàm định dạng hiển thị tiền tệ
+ * createBy: nhson(31/12/2020)
+ * @param {any} money số tiền
+ */
+function formatMoney(money) {
+    if (money == null) {
+        return "";
+    } else {
+        var num = money.toFixed(0).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1.");
+        return num;
+    }
+
+}
